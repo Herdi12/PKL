e53e9d892db7766902a46cdd1dd7536799a6a22c
FeriApriyanto17 <97430559+FeriApriyanto17@users.noreply.github.com>
2022-01-27 16:03:46 +0700

    Create loginform.php

---
+++ Aplikasi/loginform.php
@@ -0,0 +1,12 @@
+<html>
+    <form action="action_page.php"
+    method="post">
+    <div class="imgcontainer">
+        <img src="img_avatar2.png"
+        alt="avatar" class="avatar"> 
+    </div>
+     
+    <div class="container">
+        
+    </div>
+</html>
