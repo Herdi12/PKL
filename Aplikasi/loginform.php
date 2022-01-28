73ccda8b5d3122ef8f62cf6e96bbe32ee8c00541
FeriApriyanto17 <97430559+FeriApriyanto17@users.noreply.github.com>
2022-01-28 13:58:40 +0700

    Update loginform.php

--- Aplikasi/loginform.php
+++ Aplikasi/loginform.php
@@ -7,6 +7,36 @@
     </div>
      
     <div class="container">
+        <label for="uname">
+            <b>Username</b></label>
+            <input type="text"
+            placeholder="Enter Username"
+            name="uname" required>
+            
+            <label for="psw">
+              <b>password</b><label>
+                  <input type="passsword"
+                  placeholder="Enter Password"
+                  name="psw" required>
+                  
+                  <button
+                  type="submit">Login</button>
+                  <label>
+                      <input type="checkbox"
+                      checked="checked"
+                      name="remember"> Remember me 
+              </label>
+           </div>
+
+              <div class="container"
+            style="background-color:#f1f1f1">
+            <button type="button"
+            class="cancelbtn">Cancel</button>
+            <span class="psw">Fotgot <a
+            href="#">password?</a></span>
+              
+
         
     </div>
 </html>
+</form>
